7536633287255b801aeef9a1b84d8a1ea16acd7e
RianOntu <rianontu13@gmail.com>
2023-03-04 21:34:43 +0600

    created see more functionality

--- js/app.js
+++ js/app.js
@@ -1,11 +1,23 @@
-const loadCards=()=>{
+const loadCards=(dataLimit)=>{
     fetch('https://openapi.programming-hero.com/api/ai/tools')
     .then(res=>res.json())
-    .then(data=>displayCards(data.data.tools))
+    .then(data=>displayCards(data.data.tools,dataLimit))
 }
-const displayCards=cards=>{
-    const mainContainer=document.getElementById('main-container');
+const displayCards=(cards,dataLimit)=>{
     console.log(cards);
+    const mainContainer=document.getElementById('main-container');
+    mainContainer.innerText='';
+    const seeMore=document.getElementById('see-more');
+    if(dataLimit && cards.length>6){
+       
+        
+        cards=cards.slice(0,6);
+        seeMore.classList.remove('d-none')
+    }
+    else{
+        seeMore.classList.add('d-none');
+    }
+    
     cards.forEach(card => {
         const cardDiv=document.createElement('div');
         cardDiv.classList.add("col");
@@ -23,7 +35,7 @@ const displayCards=cards=>{
 
          </div>
          <div>
-         <i class="fa-solid fa-arrow-right"></i>
+        <button data-bs-toggle="modal" data-bs-target="#staticBackdrop" onclick="loadCardDetails('${card.id}')"> <i class="fa-solid fa-arrow-right"></i></button>
          </div>
 
         </div>
@@ -33,4 +45,20 @@ const displayCards=cards=>{
   
 }
 
-loadCards()
+const processSearch=dataLimit=>{
+    loadCards(dataLimit)
+}
+document.getElementById('see-more').addEventListener('click',function(){
+    processSearch()
+})
+processSearch(1)
+
+const loadCardDetails=id=>{
+fetch(`https://openapi.programming-hero.com/api/ai/tool/${id}`)
+.then(res=>res.json())
+.then(data=>displayCardDetails(data))
+}
+const displayCardDetails=data=>{
+console.log(data);
+}
+
